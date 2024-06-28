2f5a22adca509e26b21254b2cdd165f164467fbf
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-06-28 13:27:57 +0530

    Fixed issue in deployment

--- financial-fraud-detection/src/backendURL.jsx
+++ financial-fraud-detection/src/backendURL.jsx
@@ -1,2 +1,1 @@
-// export const backendURL = import.meta.env.VITE_BACKEND_URL;
-export const backendURL = "https://financial-fraud-detection.onrender.com";
+export const backendURL = import.meta.env.VITE_BACKEND_URL;
