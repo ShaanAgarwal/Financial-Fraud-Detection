667dcb2b4eea568dc9fcf44ff7ce520b40348d74
Shaan Agarwal <shaanagarwalofficial@gmail.com>
2024-06-28 13:23:05 +0530

    changed url

--- financial-fraud-detection/src/backendURL.jsx
+++ financial-fraud-detection/src/backendURL.jsx
@@ -1,2 +1,2 @@
 // export const backendURL = import.meta.env.VITE_BACKEND_URL;
-export const backendURL = "https://financial-fraud-detection.onrender.com"
+export const backendURL = "https://financial-fraud-detection.onrender.com";
